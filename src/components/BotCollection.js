8e3a3e4b1892c585f55158f63c983e8cfc80e586
MitchelleTamnai <cheptoektamnai@gmail.com>
2024-05-06 08:16:45 +0300

    Comit changes

--- src/components/BotCollection.js
+++ src/components/BotCollection.js
@@ -7,14 +7,25 @@ function BotCollection() {
         fetch("http://localhost:3000/bots")
         .then(resp => resp.json())
         .then ((bots) => {
-            console.log(bots)
+            setData({bots : bots})
         })
-        console.log(data)
+        //console.log(data)
     }, [])
 
     return (
-        <div className="card" style={{width: 18}}>
-            
+        <div className="card" style={{ width: "18rem" }}>
+            <img className="card-img-top" src={data.bots.avatar_url}  alt="Bot" />
+            <div className="card-body">
+                <ul className="card-text">
+                    <li>{data.bots.name}</li>
+                    <li>{data.bots.health}</li>
+                    <li>{data.bots.damage}</li>
+                    <li>{data.bots.armour}</li>
+                    <li>{data.bots.catchphrase}</li>
+                    <li>{data.bots.created_at}</li>
+                    <li>{data.bots.updated_at}</li>
+                </ul>
+            </div>
         </div>
     )
     
